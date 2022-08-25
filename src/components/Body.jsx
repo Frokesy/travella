ef3e791c7c1d2afc5726e0f3d95dbdbd7fd70d80
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-08-25 17:08:47 +0100

    adjusted some images

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -21,10 +21,10 @@ const Body = () => {
           </button>
       </div>
             <div className="lg:mt-6 lg:mb-0 mb-10">
-            <div className="relative flex ml-[20vw] lg:ml-0 lg:w-auto w-[90vw] mx-auto lg:mt-[6vh]">
+            <div className="relative flex ml-[18vw] lg:ml-0 lg:w-auto w-[90vw] mx-auto lg:mt-[6vh]">
                 <div className="">
-                 <img src="/assets/bodyImg__Two.svg" alt="img" className="absolute lg:top-20 top-[10vh] right-[35vw] lg:w-auto w-[80%] lg:h-[60vh] lg:right-[10vw]" /> 
-                 <img src="/assets/bodyImg__One.svg" alt="" className="absolute lg:top-[35vh] top-[18vh] lg:right-[26vw] right-[53vw] lg:h-[45vh] lg:w-auto w-[75%]" />
+                 <img src="/assets/bodyImg__Two.svg" alt="img" className="absolute lg:top-20 top-[15vh] right-[35vw] lg:w-auto w-[80%] lg:h-[60vh] lg:right-[10vw]" /> 
+                 <img src="/assets/bodyImg__One.svg" alt="" className="absolute lg:top-[35vh] top-[24vh] lg:right-[26vw] right-[53vw] lg:h-[45vh] lg:w-auto w-[75%]" />
 
                 </div>
                  <img src="/assets/bodyImg__Three.svg" alt="img" className="lg:h-[80vh] lg:w-auto w-[75vw] w-auto" />
